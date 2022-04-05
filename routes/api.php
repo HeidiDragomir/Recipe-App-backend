cc34e4efdca291a9c56d1308d04e748f28a2d744
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-04-05 06:29:55 +0200

    Create all routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RecipeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+// Public routes
+/* Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']); */
+Route::get('/recipes', [RecipeController::class, 'getAllRecipes']);
+Route::get('/recipes/{id}', [RecipeController::class, 'getRecipe']);
+// Route::get('/recipes/search/{name}', [RecipeController::class, 'search']);
+
+Route::post('/recipes', [RecipeController::class, 'createRecipe']);
+// Protected routes
+/* Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/recipes', [RecipeController::class, 'createRecipe']);
+    Route::put('/recipes/{id}', [RecipeController::class, 'updateRecipe']);
+    Route::delete('/recipes/{id}', [RecipeController::class, 'deleteRecipe']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+}); */
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
