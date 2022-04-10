2e0d2048552cd67d217f3d18518006d6ae2c2c6e
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-04-10 20:51:47 +0200

    Add store function for lists

--- app/Http/Controllers/RlistController.php
+++ app/Http/Controllers/RlistController.php
@@ -7,39 +7,47 @@ use Illuminate\Http\Request;
 
 class RlistController extends Controller
 {
+
     public function store(Request $request)
     {
         if (Rlist::where('recipe_id', $request->recipe_id)->where('recipe_list_id', $request->recipe_list_id)->exists()) {
-            return back()->with([
-                'success' => 'Recipe is already in that list!',
-                'color' => 'danger'
-            ]);
+            return response()->json([
+                "message" => "Recipe is already in that list!"
+            ], 409);
         } else {
-            $attributes = request()->validate([
-                'recipe_list_id' => 'required',
-                'recipe_id' => 'required'
-            ]);
-            Rlist::create($attributes);
-            return back()->with([
-                'success' => 'Recipe added to list!',
-                'color' => 'success'
-            ]);
+
+            if (Rlist::where('recipe_list_id', $request->recipe_list_id)->exists()) {
+
+                $attributes = request()->validate([
+                    'recipe_list_id' => 'required',
+                    'recipe_id' => 'required'
+                ]);
+                Rlist::create($attributes);
+                return response()->json([
+                    "message" => "Recipe added to list"
+                ], 201);
+            } else {
+                return response()->json([
+                    "message" => "List not found"
+                ], 404);
+            }
         }
     }
 
 
+    // NU MERGE
 
     public function destroy()
     {
 
-        $recipeId = request('recipe_id');
-        $recipelistId = request('recipe_list_id');
+        $recipe_id = request('recipe_id');
+        $recipe_list_id = request('recipe_list_id');
 
 
-        Rlist::where('recipe_id', $recipeId)->where('recipe_list_id', $recipelistId)->delete();
-        return back()->with([
-            'success' => 'Recipe Deleted From List!',
-            'color' => 'danger'
-        ]);
+        Rlist::where('recipe_id', $recipe_id)->where('recipe_list_id', $recipe_list_id)->delete();
+
+        return response()->json([
+            "message" => "Record deleted"
+        ], 201);
     }
 }
